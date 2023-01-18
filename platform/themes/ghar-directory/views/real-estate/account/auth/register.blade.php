0549e8d50d078c2d6343b5cf4cfe9fa614986158
dev <dev@example.com>
2023-01-18 16:53:50 +0530

    on register page add eye icon to show password

--- platform/themes/ghar-directory/views/real-estate/account/auth/register.blade.php
+++ platform/themes/ghar-directory/views/real-estate/account/auth/register.blade.php
@@ -70,6 +70,12 @@
                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
                                    name="password" required
                                    placeholder="{{ trans('plugins/real-estate::dashboard.password') }}">
+                                    <span style="  float: right;
+                                    margin-left: -25px;
+                                    margin-top: -26px;
+                                    position: relative;
+                                    font-size: large;
+                                    z-index: 2;"  class="fa fa-fw fa-eye-slash viewpass mr-4 text-muted" onclick="handle('password')" id="eyeicon"></span>
                             @if ($errors->has('password'))
                                 <span class="invalid-feedback">
                                     <strong>{{ $errors->first('password') }}</strong>
@@ -81,6 +87,12 @@
                             <input id="password-confirm" type="password" class="form-control"
                                    name="password_confirmation" required
                                    placeholder="{{ trans('plugins/real-estate::dashboard.password-confirmation') }}">
+                                   <span style="  float: right;
+                                    margin-left: -25px;
+                                    margin-top: -26px;
+                                    position: relative;
+                                    font-size: large;
+                                    z-index: 2;" class="fa fa-fw fa-eye-slash viewpass mr-4 text-muted" onclick="handle('confirm_password')" id="eyeicon_confirm"></span>
                         </div>
 
                         @if (is_plugin_active('captcha') && setting('enable_captcha') && setting('real_estate_enable_recaptcha_in_register_page', 0))
@@ -108,3 +120,25 @@
         </div>
     </div>
 </div>
+<script>
+
+function handle(type) {
+    if(type == 'password'){
+        var password = document.getElementById('password')
+        var eyeicon = $('#eyeicon')
+    }
+    else{
+        var password = document.getElementById('password-confirm')
+        var eyeicon = $('#eyeicon_confirm')
+    }
+
+    if (password.type == "password") {
+        password.type = "text"
+        eyeicon.removeClass('fa-eye-slash').addClass('fa-eye');
+    } else {
+        password.type = "password"
+        eyeicon.removeClass('fa-eye').addClass('fa-eye-slash');
+    }
+}
+
+</script>
