c02b588770cfe8751644d50816de246f41f752fe
dev <dev@example.com>
2023-01-19 18:01:04 +0530

    validation

--- platform/themes/ghar-directory/public/js/wishlist.js
+++ platform/themes/ghar-directory/public/js/wishlist.js
@@ -1,1 +1,166 @@
-!function(i){"use strict";var t=function(i){window.showAlert("alert-success",i)},n=function(i){return window.trans=window.trans||{},"undefined"!==window.trans[i]&&window.trans[i]?window.trans[i]:i};window.showAlert=function(t,n){if(t&&""!==n){var a=Math.floor(1e3*Math.random()),e='<div class="alert '.concat(t,' alert-dismissible" id="').concat(a,'">\n                            <span class="close far fa-times" data-dismiss="alert" aria-label="close"></span>\n                            <i class="far fa-')+("alert-success"===t?"check":"times")+' message-icon"></i>\n                            '.concat(n,"\n                        </div>");i("#alert-container").append(e).ready((function(){window.setTimeout((function(){i("#alert-container #".concat(a)).remove()}),6e3)}))}},i(document).ready((function(){function a(){var t=window.currentLanguage+"_wishlist",n=decodeURIComponent(s(t));if(null!=n&&null!=n&&n){var a=JSON.parse(n),e=a.length;i(".wishlist-count").text(e),e>0&&(i(".add-to-wishlist").removeClass("far fa-heart"),i.each(a,(function(t,n){null!=n&&i(document).find(".add-to-wishlist[data-id=".concat(n.id,"] i")).addClass("fas fa-heart")})))}}function e(i,t,n){var a=new Date,e=window.siteUrl;e.includes(window.location.protocol)||(e=window.location.protocol+e);var s=new URL(e);a.setTime(a.getTime()+24*n*60*60*1e3);var o="expires="+a.toUTCString();document.cookie=i+"="+t+"; "+o+"; path=/; domain="+s.hostname}function s(i){for(var t=i+"=",n=document.cookie.split(";"),a=0;a<n.length;a++){for(var e=n[a];" "==e.charAt(0);)e=e.substring(1);if(0==e.indexOf(t))return e.substring(t.length,e.length)}return""}function o(i){var t=window.siteUrl;t.includes(window.location.protocol)||(t=window.location.protocol+t);var n=new URL(t);document.cookie=i+"=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain="+n.hostname}a(),i(document).on("click",".add-to-wishlist",(function(r){r.preventDefault();var d=window.currentLanguage+"_wishlist",l=i(this).data("id"),c=decodeURIComponent(s(d)),u=[];if(null!=l&&0!=l&&null!=l)if(null==c||null==c||""==c){var f={id:l};u.push(f),i(".add-to-wishlist[data-id=".concat(l,"] i")).removeClass("far fa-heart").addClass("fas fa-heart"),t(n("Added to wishlist successfully!")),e(d,JSON.stringify(u),60)}else{var w={id:l},h=(u=JSON.parse(c)).map((function(i){return i.id})).indexOf(w.id);-1===h?(u.push(w),o(d),e(d,JSON.stringify(u),60),i(".add-to-wishlist[data-id=".concat(l,"] i")).removeClass("far fa-heart").addClass("fas fa-heart"),t(n("Added to wishlist successfully!"))):(u.splice(h,1),o(d),e(d,JSON.stringify(u),60),i(".add-to-wishlist[data-id=".concat(l,"] i")).removeClass("fas fa-heart").addClass("far fa-heart"),t(n("Removed from wishlist successfully!")))}var m=JSON.parse(s(d)).length;i(".wishlist-count").text(m),a()})),i(document).on("click",".remove-from-wishlist",(function(r){r.preventDefault();var d=window.currentLanguage+"_wishlist",l=i(this).data("id"),c=decodeURIComponent(s(d)),u=[];if(null!=l&&0!=l&&null!=l){var f={id:l},w=(u=JSON.parse(c)).map((function(i){return i.id})).indexOf(f.id);-1!=w&&(u.splice(w,1),o(d),e(d,JSON.stringify(u),60),t(n("Removed from wishlist successfully!")),i(".wishlist-page .item[data-id=".concat(l,"]")).closest("div").remove())}var h=JSON.parse(s(d)).length;i(".wishlist-count").text(h),a()})),window.wishlishInElement=function(t){var n=JSON.parse(s(window.currentLanguage+"_wishlist")||"{}");n&&n.length&&t.find(".add-to-wishlist").map((function(){var t=i(this).data("id");n.some((function(i){return i.id===t}))&&i(this).find("i").addClass("fas")}))}}))}(jQuery);
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************************************!*\
+  !*** ./platform/themes/ghar-directory/assets/js/wishlist.js ***!
+  \**************************************************************/
+(function ($) {
+  'use strict';
+
+  var showSuccess = function showSuccess(message) {
+    window.showAlert('alert-success', message);
+  };
+  var __ = function __(key) {
+    window.trans = window.trans || {};
+    return window.trans[key] !== 'undefined' && window.trans[key] ? window.trans[key] : key;
+  };
+  window.showAlert = function (messageType, message) {
+    if (messageType && message !== '') {
+      var alertId = Math.floor(Math.random() * 1000);
+      var html = "<div class=\"alert ".concat(messageType, " alert-dismissible\" id=\"").concat(alertId, "\">\n                            <span class=\"close far fa-times\" data-dismiss=\"alert\" aria-label=\"close\"></span>\n                            <i class=\"far fa-") + (messageType === 'alert-success' ? 'check' : 'times') + " message-icon\"></i>\n                            ".concat(message, "\n                        </div>");
+      $('#alert-container').append(html).ready(function () {
+        window.setTimeout(function () {
+          $("#alert-container #".concat(alertId)).remove();
+        }, 6000);
+      });
+    }
+  };
+  $(document).ready(function () {
+    setWishListCount();
+    $(document).on('click', '.add-to-wishlist', function (e) {
+      e.preventDefault();
+      var cookieName = window.currentLanguage + '_wishlist';
+      var property_id = $(this).data('id');
+      var wishCookies = decodeURIComponent(getCookie(cookieName));
+      var arrWList = [];
+      if (property_id != null && property_id != 0 && property_id != undefined) {
+        // Case 1: Wishlist cookies are undefined
+        if (wishCookies == undefined || wishCookies == null || wishCookies == '') {
+          var item = {
+            id: property_id
+          };
+          arrWList.push(item);
+          $(".add-to-wishlist[data-id=".concat(property_id, "] i")).removeClass('far fa-heart').addClass('fas fa-heart');
+          showSuccess(__('Added to wishlist successfully!'));
+          setCookie(cookieName, JSON.stringify(arrWList), 60);
+        } else {
+          var _item = {
+            id: property_id
+          };
+          arrWList = JSON.parse(wishCookies);
+          var index = arrWList.map(function (e) {
+            return e.id;
+          }).indexOf(_item.id);
+          if (index === -1) {
+            arrWList.push(_item);
+            clearCookies(cookieName);
+            setCookie(cookieName, JSON.stringify(arrWList), 60);
+            $(".add-to-wishlist[data-id=".concat(property_id, "] i")).removeClass('far fa-heart').addClass('fas fa-heart');
+            showSuccess(__('Added to wishlist successfully!'));
+          } else {
+            arrWList.splice(index, 1);
+            clearCookies(cookieName);
+            setCookie(cookieName, JSON.stringify(arrWList), 60);
+            $(".add-to-wishlist[data-id=".concat(property_id, "] i")).removeClass('fas fa-heart').addClass('far fa-heart');
+            showSuccess(__('Removed from wishlist successfully!'));
+          }
+        }
+      }
+      var countWishlist = JSON.parse(getCookie(cookieName)).length;
+      $('.wishlist-count').text(countWishlist);
+      setWishListCount();
+    });
+    $(document).on('click', '.remove-from-wishlist', function (e) {
+      e.preventDefault();
+      var cookieName = window.currentLanguage + '_wishlist';
+      var property_id = $(this).data('id');
+      var wishCookies = decodeURIComponent(getCookie(cookieName));
+      var arrWList = [];
+      if (property_id != null && property_id != 0 && property_id != undefined) {
+        var item = {
+          id: property_id
+        };
+        arrWList = JSON.parse(wishCookies);
+        var index = arrWList.map(function (e) {
+          return e.id;
+        }).indexOf(item.id);
+        if (index != -1) {
+          arrWList.splice(index, 1);
+          clearCookies(cookieName);
+          setCookie(cookieName, JSON.stringify(arrWList), 60);
+          showSuccess(__('Removed from wishlist successfully!'));
+          $(".wishlist-page .item[data-id=".concat(property_id, "]")).closest('div').remove();
+        }
+      }
+      var countWishlist = JSON.parse(getCookie(cookieName)).length;
+      $('.wishlist-count').text(countWishlist);
+      setWishListCount();
+    });
+    function setWishListCount() {
+      var cookieName = window.currentLanguage + '_wishlist';
+      var wishListCookies = decodeURIComponent(getCookie(cookieName));
+      if (wishListCookies != null && wishListCookies != undefined && !!wishListCookies) {
+        var arrList = JSON.parse(wishListCookies);
+        var countWishlist = arrList.length;
+        $('.wishlist-count').text(countWishlist);
+        if (countWishlist > 0) {
+          $('.add-to-wishlist').removeClass('far fa-heart');
+          $.each(arrList, function (key, value) {
+            if (value != null) {
+              $(document).find(".add-to-wishlist[data-id=".concat(value.id, "] i")).addClass('fas fa-heart');
+            }
+          });
+        }
+      }
+    }
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      var siteUrl = window.siteUrl;
+      if (!siteUrl.includes(window.location.protocol)) {
+        siteUrl = window.location.protocol + siteUrl;
+      }
+      var url = new URL(siteUrl);
+      d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+      var expires = 'expires=' + d.toUTCString();
+      document.cookie = cname + '=' + cvalue + '; ' + expires + '; path=/' + '; domain=' + url.hostname;
+    }
+    function getCookie(cname) {
+      var name = cname + '=';
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return '';
+    }
+    function clearCookies(name) {
+      var siteUrl = window.siteUrl;
+      if (!siteUrl.includes(window.location.protocol)) {
+        siteUrl = window.location.protocol + siteUrl;
+      }
+      var url = new URL(siteUrl);
+      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/' + '; domain=' + url.hostname;
+    }
+    function checkWishlistInElement($el) {
+      var parseCookie = JSON.parse(getCookie(window.currentLanguage + '_wishlist') || '{}');
+      if (parseCookie && parseCookie.length) {
+        $el.find('.add-to-wishlist').map(function () {
+          var wlId = $(this).data('id');
+          var exists = parseCookie.some(function (x) {
+            return x.id === wlId;
+          });
+          if (exists) {
+            $(this).find('i').addClass('fas');
+          }
+        });
+      }
+    }
+    window.wishlishInElement = checkWishlistInElement;
+  });
+})(jQuery);
+/******/ })()
+;
