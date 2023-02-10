6988a6935aa0a2d357153b7ca3d0c45b5e0e6cea
dev <dev@example.com>
2023-02-10 20:05:51 +0530

    regoister page changes

--- platform/themes/ghar-directory/partials/real-estate/agents/item.blade.php
+++ platform/themes/ghar-directory/partials/real-estate/agents/item.blade.php
@@ -15,10 +15,12 @@
         <div class="fr-grid-detail">
             <div class="fr-grid-detail-flex">
                 <h5 class="fr-can-name">
-                    @if ($account->username)
+                    @if ($account->username && $account->fff_name)
+                        <a href="{{ route('public.agent', $account->username) }}">{{ $account->fff_name }}</a>
+                    @elseif($account->username && $account->name)
                         <a href="{{ route('public.agent', $account->username) }}">{{ $account->name }}</a>
                     @else
-                        {{ $account->name }}
+                        {{ $account->fff_name }}
                     @endif
                 </h5>
             </div>
