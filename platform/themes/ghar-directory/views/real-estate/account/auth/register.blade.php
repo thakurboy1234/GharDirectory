a605b1cf5e3885a23e7f4160a4609cfaf4f0df13
dev <dev@example.com>
2023-02-10 18:57:01 +0530

    regoister page changes

--- platform/themes/ghar-directory/views/real-estate/account/auth/register.blade.php
+++ platform/themes/ghar-directory/views/real-estate/account/auth/register.blade.php
@@ -44,36 +44,14 @@
                             @endif
                         </div>
                         <div class="form-group mb-3">
-                            <input id="company" type="text"
-                                   class="form-control{{ $errors->has('company') ? ' is-invalid' : '' }}"
-                                   name="company" value="{{ old('company') }}"
-                                   placeholder="{{ trans('plugins/real-estate::dashboard.company') }}">
-                            @if ($errors->has('company'))
-                                <span class="invalid-feedback">
-                                <strong>{{ $errors->first('company') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="form-group mb-3">
                             <input id="phone" type="text"
-                                   class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}"
-                                   name="phone" value="{{ old('phone') }}" required
-                                   placeholder="{{ trans('plugins/real-estate::dashboard.phone') }}">
+                            class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}"
+                            name="phone" value="{{ old('phone') }}" required
+                            placeholder="{{ trans('plugins/real-estate::dashboard.phone') }}">
                             @if ($errors->has('phone'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('phone') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                        <div class="form-group mb-3">
-                            <input id="alternate_mobile_number" type="text"
-                                   class="form-control{{ $errors->has('alternate_mobile_number') ? ' is-invalid' : '' }}"
-                                   name="alternate_mobile_number" value="{{ old('alternate_mobile_number') }}"
-                                   placeholder="{{ trans('plugins/real-estate::dashboard.alternate_mobile_number') }}">
-                            @if ($errors->has('alternate_mobile_number'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('alternate_mobile_number') }}</strong>
-                                    </span>
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('phone') }}</strong>
+                            </span>
                             @endif
                         </div>
                         <div class="form-group mb-3">
@@ -86,6 +64,28 @@
                                 @endif
                             </select>
 
+                        </div>
+                        <div class="form-group mb-3">
+                            <input id="company" type="text"
+                                   class="form-control{{ $errors->has('company') ? ' is-invalid' : '' }}"
+                                   name="company" value="{{ old('company') }}"
+                                   placeholder="{{ trans('plugins/real-estate::dashboard.company') }}">
+                            @if ($errors->has('company'))
+                                <span class="invalid-feedback">
+                                <strong>{{ $errors->first('company') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                        <div class="form-group mb-3">
+                            <input id="alternate_mobile_number" type="text"
+                                   class="form-control{{ $errors->has('alternate_mobile_number') ? ' is-invalid' : '' }}"
+                                   name="alternate_mobile_number" value="{{ old('alternate_mobile_number') }}"
+                                   placeholder="{{ trans('plugins/real-estate::dashboard.alternate_mobile_number') }}">
+                            @if ($errors->has('alternate_mobile_number'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('alternate_mobile_number') }}</strong>
+                                    </span>
+                            @endif
                         </div>
 
                         <div class="form-group">
@@ -224,6 +224,9 @@
             },
             password_confirmation: {
                 required: "Confirm-Password is required.",
+            },
+            city_id: {
+                required: "City is required.",
             },
         },
 
