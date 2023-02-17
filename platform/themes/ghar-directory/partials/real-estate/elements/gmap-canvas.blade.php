39e7d60ea3c7d421a972ee5cca4048d48e507fe9
dev <dev@example.com>
2023-02-17 12:12:11 +0530

    map show defalut location india

--- platform/themes/ghar-directory/partials/real-estate/elements/gmap-canvas.blade.php
+++ platform/themes/ghar-directory/partials/real-estate/elements/gmap-canvas.blade.php
@@ -1,5 +1,5 @@
 <div class="gmap_canvas mt-1 my-3">
     <iframe id="gmap_canvas" width="100%" height="100%"
-            src="https://maps.google.com/maps?q={{ urlencode($location) }}%20&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            src="https://maps.google.com/maps?q=india%20&t=&z=5&ie=UTF8&iwloc=&output=embed"
             frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 </div>
